c71cf3a278b0353942277d6f1108539dca902e54
TheMrPixelDev <julian.harrer@posteo.de>
2023-04-12 13:34:22 +0200

    Fixed word warp on headline

---
+++ src/lib/BillboardHeader.svelte
@@ -0,0 +1,13 @@
+<h3>
+    Du hast bock auf Party? Hier hast du welche!
+</h3>
+
+<style>
+    h3 {
+        text-decoration: underline;
+        margin: 6rem 1rem 0 1rem;
+        text-align: center;
+        word-wrap: normal;
+    }
+</style>
+  
